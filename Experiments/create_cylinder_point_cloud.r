9a70b626d3d6cb7fbb5d17f174933c6a002b6ad5
Ethan Peacock <peacockethan@gmail.com>
2021-04-11 20:32:26 -0400

    Pull out sub functions and lint code

--- Experiments/create_cylinder_point_cloud.r
+++ Experiments/create_cylinder_point_cloud.r
@@ -1,12 +1,13 @@
-validate_get_cylinder_args <- function(radius, height, num_around, num_up,
+validate_get_cylinder_args <- function(radius, height, num_around, num_stacks,
                                        lid_step) {
-    #   Checks to make sure inputs to get_cylinder are logical
+    #   Checks to make sure inputs to get_cylinder are logical.
+    #   helper function to get_cylinder()
 
     #   Args:
     #       radius (double): ............ cylinder radius
     #       height (double): ............ cylinder height
     #       num_around (int): ........... number of points around circle
-    #       num_up (int): ............... number of circles to be stacked
+    #       num_stacks (int): ........... number of circles to be stacked
     #
     #   Returns:
     #       The program is stopped if an input is found to be invalid.
@@ -20,17 +21,18 @@ validate_get_cylinder_args <- function(radius, height, num_around, num_up,
     # must have at least 3 points to create surfae
     if (num_around < 3)
         stop("num_around MUST be at least 3.")
-    # num_up must be at least 2
-    if (num_up < 2)
-        stop("num_up MUST be at least 2.")
+    # num_stacks must be at least 2
+    if (num_stacks < 2)
+        stop("num_stacks MUST be at least 2.")
     # lid step must be negative to converge on center point
-    if (lid_step > 0)
-        stop("lid_step MUST be negative step size.")
+    if (lid_step < 0 || lid_step >=1 )
+        stop("lid_step MUST be small positive step size.")
 }
 
 
 get_circle <- function(radius, num_around) {
     #   Gets x, y coordinates for a circle with radius and number of points.
+    #   helper function to get_cylinder()
 
     #   Args:
     #       radius (double):  ..... cylinder radius
@@ -54,14 +56,14 @@ get_circle <- function(radius, num_around) {
 }
 
 
-get_cylinder <- function(radius=1, height=10, num_around=25, num_up=10,
-                         lids=TRUE, lid_step=-0.2) {
+get_cylinder <- function(radius=1, height=10, num_around=25, num_stacks=10,
+                         lids=TRUE, lid_step=0.2) {
     #   Creates cylinder point cloud with various options.
 
     #   Args:
     #       radius (double): ... radius for the cylinder base
     #	    num_aroud (int): ... number of vertices in circle to stack
-    #	    num_up (int): ...... number of circles we stack to make the cylinder
+    #	    num_stacks (int): .. number of circles we stack to make the cylinder
     #	    height (double): ... height of the cylinder
     #	    lids (boolean): .... closed cylinder if TRUE, open pipe if FALSE
     #	    lid_step: .......... step size for lid radius change
@@ -70,32 +72,32 @@ get_cylinder <- function(radius=1, height=10, num_around=25, num_up=10,
     #       The point cloud cylinder is returned as a data frame x, y, z.
 
     # validate arguments given before building cylinder
-    validate_get_cylinder_args(radius, height, num_around, num_up, lid_step)
+    validate_get_cylinder_args(radius, height, num_around, num_stacks, lid_step)
 
-    # ________________________________________________________ build cylinder __
-
-    # get circle for stacking in cylinder
+    # get circle for specified radius and discretization number
     circle_base <- get_circle(radius, num_around)
 
     # set the ground as 0
     ground <- 0
-    # chop up z dimension so we have height steps from the ground (0) to
-    # the top of our cylinder, effectively we are stacking the circles
+
+    # discretize z dimension from ground 0 to top of cylinder
     z_steps <- seq(from <- ground, to <- height,
-                    by <- ((height - ground) / (num_up - 1)))
+                    by <- ((height - ground) / (num_stacks - 1)))
 
     # initialize empty data frame for holding our cylinder of stacked circles
     cylinder <- c()
+
     # build cylinder by stacking circles
     for (z in z_steps) {
         # if it's our bottom circle or top circle, and lids is TRUE,
-        # we have to fill it in creating a full cylinder, not just a pipe
-        if ((lids == TRUE && z == z_steps[1]) ||
+        # we have to fill it in creating a circular plane, not a hollow circle
+        if ((lids == TRUE && z == ground) ||
             (lids == TRUE && z == height)) {
 
-            # get sequence of converting radius so we can create the lids
-            radii <- seq(radius, 0, lid_step)
-            # loop through radii and create lids for cylinder
+            # get sequence of diverging radius so we can create the lids
+            radii <- seq(radius, 0, -lid_step)
+
+            # loop through radius in seq and create top or bottom cylinder
             for (radius_i in radii) {
                 # get points for caps (lids) on cylinder
                 circle_lid <- get_circle(radius_i, num_around)
@@ -124,36 +126,53 @@ get_cylinder <- function(radius=1, height=10, num_around=25, num_up=10,
 }
 
 
-main <- function() {
+plot_cylinder <- function(cylinder, color) {
+    #   Uses rgl to plot cylinder
+    #
+    #   Args:
+    #       cylinder(3d point cloud): ........ the cylinder from get_cylinder()
+    #       color (string): .................. color for points
+    #   Returns:
+    #       Rgl opens a plot window and displays the point cloud.
+
     # rgl for plot3d
     require(rgl)
 
-    # get cylinder with default arguments
-    cylinder <- get_cylinder()
     # plot
     rgl::open3d()
-    rgl::plot3d(cylinder, col = "blue")
+    rgl::plot3d(cylinder, col = color)
+}
 
-    # set values for custom cylinder
-    # We want a cylinder with radius=0.25 and height = 10
-    radius <- 0.25          # cylinder radius
-    height <- 10            # cylinder height
-    num_around <- 360       # number of points around circle for cylinder pipe
-    num_stacked <- 720      # number of circles stacked to create cylinder pipe
-    lids <- TRUE            # We want full cylinder with lids (top, bottom)
-    lid_step <- -0.01       # step size for lids converging on center
 
-    # create cylinder using parameters above
-    cylinder_for_mesh <- get_cylinder(radius, height, num_around,
-                                        num_stacked, lids, lid_step)
-    # plot
-    rgl::open3d()
-    rgl::plot3d(cylinder_for_mesh, col = "red")
-
-    # save cylinder as csv for meshing
-    write.csv(cylinder_for_mesh,
-            file = "~/Desktop/cylinder_rad0.25_height10_withlids.xyz",
+save_cylinder <- function(cylinder, file_name) {
+    write.csv(cylinder,
+            file = file_name,
             row.names = FALSE)
+}
+
+
+main <- function() {
+    # build cylinder using default arguments
+    cylinder_default <- get_cylinder()
+    # build cylinder with surface area of 16.1
+    radius = 0.25
+    height = 10
+    num_around = 720
+    num_stacks = 1200
+    cylinder_for_mesh <- get_cylinder(radius, height, num_around, num_stacks, 
+                                      lid_step=0.02)
+
+    # plot cylinder with default arguments
+    plot_cylinder(cylinder_default, "blue")
+    # plot cylinder with surface area of 16.1
+    plot_cylinder(cylinder_for_mesh, "red")
+
+
+    # save cylinder with surface area of 16.1 radius=0.25 height=10
+    save_path <- paste("~/Desktop/cylinder_w_surf_area_16.1_",
+                       "radius_0.25_height_10.xyz", sep="")
+
+    save_cylinder(cylinder_for_mesh, save_path)
 }
 
 
