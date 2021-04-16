da13dd26b16a1cca03d8c1db399834c0bc6a3e66
Ethan Peacock <peacockethan@gmail.com>
2021-04-16 10:30:13 -0400

    Add time library; add save mesh
    * use time library to calculate cpu time of poisson
    * save mesh out to experiment directory

--- Experiments/Exp5/poisson_surface_reconstruction.py
+++ Experiments/Exp5/poisson_surface_reconstruction.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import open3d as o3d
+import time
 
 if __name__ == "__main__":
     # set cylinder surface area for height = 10, radius = 0.25
@@ -11,22 +12,33 @@ if __name__ == "__main__":
     cylinder_file = ("Experiments/Data/cylinder_r.25_h10_normals_" +
                      "point_cloud.ply")
 
-
     # create point cloud object from cylinder file
     pc = o3d.io.read_point_cloud(cylinder_file)
 
-    # visualize
-    o3d.visualization.draw_geometries([pc], point_show_normal=True)
+    # # visualize
+    # o3d.visualization.draw_geometries([pc], point_show_normal=True)
 
+    # start timer
+    start = time.time()
     # perform poisson surface reconstruction
     poisson_mesh, dens = o3d.geometry.TriangleMesh \
                                      .create_from_point_cloud_poisson(pc)
 
-    # visualize mesh
-    o3d.visualization.draw_geometries([poisson_mesh])
+    # stop timer
+    stop = time.time()
+    # calculate time
+    run_time = stop - start
+    print(f"Time: {run_time}")
+
+    # # visualize mesh
+    # o3d.visualization.draw_geometries([poisson_mesh])
 
     # grab surface area of triangulated mesh
     surface_area_poisson = o3d.geometry.TriangleMesh \
                                        .get_surface_area(poisson_mesh)
 
     print(f"Surface Area of Mesh: {surface_area_poisson}")
+    
+    # save poisson mesh
+    save_path = "Experiments/Exp5/MeshCylinders/poisson_cylinder_mesh.ply"
+    o3d.io.write_triangle_mesh(save_path, poisson_mesh, write_ascii=True)
