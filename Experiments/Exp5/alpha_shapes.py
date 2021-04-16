e285d812acb73c11648e1ab0e6432c1c2d821ffb
Ethan Peacock <peacockethan@gmail.com>
2021-04-16 09:56:58 -0400

    Add dirty script for alpha shapes
    * read in cylinder point cloud with
    * height = 10
    * radius = 0.25
    * surface area = 16.1
    * perform alpha shape delaunay tetrahedralization on cylinder

---
+++ Experiments/Exp5/alpha_shapes.py
@@ -0,0 +1,35 @@
+# TODO: add shebangg
+import numpy as np
+import open3d as o3d
+
+
+if __name__ == "__main__":
+    # set cylinder surface area for height = 10, radius = 0.25
+    CYLINDER_SURFACE_AREA = 16.1
+
+    # read in point cloud for cylinder
+    cylinder_file = ("Experiments/Data/cylinder_r.25_h10_normals_" +
+                     "point_cloud.ply")
+
+    # create point cloud object from cylinder file
+    pc = o3d.io.read_point_cloud(cylinder_file)
+
+    # # visualize
+    # o3d.visualization.draw_geometries([pc], point_show_normal=True)
+
+    # mesh point cloud cylinder using alpha shape approach
+    alpha = 0.1
+    print(f"alpha={alpha:.3f}")
+    with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as cm:
+        alpha_mesh = o3d.geometry.TriangleMesh \
+                                .create_from_point_cloud_alpha_shape(pc, 
+                                                                    alpha)
+
+    # view alpha shape mesh
+    o3d.visualization.draw_geometries([alpha_mesh])
+
+    # get surface area of alpha triangle mesh
+    alpha_surface_area = o3d.geometry.TriangleMesh.get_surface_area(alpha_mesh)
+
+    print("Alpha shape surface area: {alpha_surface_area}")
