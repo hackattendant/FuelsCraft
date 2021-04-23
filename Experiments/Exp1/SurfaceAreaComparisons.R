2a9e5903aee40fc8433dfe4c90f98118c3067fb3
Ethan Peacock <peacockethan@gmail.com>
2021-04-23 02:46:23 -0400

    Add dirty experiment needs cleaning for experiment 1

--- Experiments/Exp1/SurfaceAreaComparisons.R
+++ Experiments/Exp1/SurfaceAreaComparisons.R
@@ -11,10 +11,10 @@ library(MASS)
 rgl.clear()
 
 # set working directory
-setwd("~/Desktop/Root/Projects/FuelsCraft/R_Meshing_Playground")
+setwd("~/Desktop/subsets/")
 
 
-totalSurfaceArea <- 0
+total_surface_area <- 0
 
 # build loop that reads through files
 # 0 to 45 because of subsets in directory
@@ -22,9 +22,9 @@ cat("\nBeginning Surface Area Calculations\n",
     "_______________________________________________________________________")
 for (n in 0:74) {
   # build file path
-  file_to_read <- "~/Desktop/Root/Projects/FuelsCraft/R_Meshing_Playground/freshSubsets/freshSubSets"
-  file_to_read <- (paste(file_to_read, toString(n)))
-  file_to_read <- (paste(file_to_read, ".xyz"))
+  file_to_read <- "subset"
+  file_to_read <- (paste(file_to_read, toString(n), sep=""))
+  file_to_read <- (paste(file_to_read, ".xyz", sep=""))
   #print(file_to_read)
   
   # read in file
@@ -32,7 +32,6 @@ for (n in 0:74) {
   
   # subset format is label, x, y, z
   # strip out xyz
-  data <- data[,2:4]
   
   # maybe TODO
   # if data doesn't contain enough points
@@ -49,11 +48,11 @@ for (n in 0:74) {
   # increase threshold proportionally to scale of size of data you are meshing
 
   # normalize x and y channels
-  data[,1] <- (data[,1]-min(data[,1]))
-  data[,2] <- (data[,2]-min(data[,2]))
+  #data[,1] <- (data[,1]-min(data[,1]))
+  #data[,2] <- (data[,2]-min(data[,2]))
   
   # run kde
-  dens <- kde3d(data[,1], data[,2], data[,3], n=100)
+  dens <- kde3d(data[,1], data[,2], data[,3], n=20)
   
   # perform meshing
   storage.mode(dens$d) <- "integer"
@@ -66,7 +65,7 @@ for (n in 0:74) {
   # calculate surface area of current mesh
   surfArea = (vcgArea(mesh)/2)
   # add surface area to total
-  totalSurfaceArea <- (totalSurfaceArea + surfArea)
+  total_surface_area <- (total_surface_area + surfArea)
   # report surface area in console
   cat("File ")
   cat(toString(n))
@@ -79,26 +78,26 @@ for (n in 0:74) {
   shade3d(mesh)
 }
 # display total surface area calculated
-cat("\nTotal Surface Area Using subsets: ", totalSurfaceArea)
+cat("\nTotal Surface Area Using subsets: ", total_surface_area)
 cat("\n\n")
 
 
 
 # _______________ calculate surface area of entire tree tip
-data = read.csv("~/Desktop/Root/Projects/FuelsCraft/R_Meshing_Playground/tipOfTreeCleaned.xyz")
+data = read.csv("~/Desktop/FuelsCraft/R_Meshing_Playground/tipOfTree.txt")
 # strip out xyz
 data <- data[,1:3]
 # normalize x and y channels
-data[,1] <- (data[,1]-min(data[,1]))
-data[,2] <- (data[,2]-min(data[,2]))
-data[,3] <- (data[,3]-min(data[,3]))
+#data[,1] <- (data[,1]-min(data[,1]))
+#data[,2] <- (data[,2]-min(data[,2]))
+#data[,3] <- (data[,3]-min(data[,3]))
 
 # run kde
 # n is number of grid points in each dimension
 # defining limits could be useful
 # can we plot density output from kde?
 # TODO: here
-dens <- kde3d(data[,1], data[,2], data[,3], n=100)
+dens <- kde3d(data[,1], data[,2], data[,3], n=20)
 # get dimensionality for entire tip and change thresh until surface area calcs are about same
 # for 10cm subsets vs full 
 # density at 50 at 10cm vs entire top of tree get range from all dimensions lowest to highest
@@ -113,8 +112,9 @@ dens <- kde3d(data[,1], data[,2], data[,3], n=100)
 # legend colors and iterations and groups of thresholds
 # perform meshing
 storage.mode(dens$d) <- "integer"
-mesh <- vcgIsosurface(dens$d, threshold=50)
+mesh <- vcgIsosurface(dens$d, threshold=100)
 surfAreaWhole = (vcgArea(mesh)/2)
 
 cat("\n Surface area of entire point cloud: ", surfAreaWhole)
 cat("\n\n")
+
